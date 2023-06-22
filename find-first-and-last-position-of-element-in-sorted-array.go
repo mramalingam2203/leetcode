d51f7c3d4f6ad300163623c0bc229a6b3311f273
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-22 15:05:41 +0530

    return indices of first and last occurrence

--- find-first-and-last-position-of-element-in-sorted-array.go
+++ find-first-and-last-position-of-element-in-sorted-array.go
@@ -1,6 +1,3 @@
-// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-
-
 package main
 
 import (
@@ -8,4 +5,50 @@ import (
 	"os"
 )
 
+func searchRange(nums []int, target int) []int {
 
+	if len(nums) < 0 || len(nums) > 100000 {
+		fmt.Println("Array length out of range")
+		os.Exit(0)
+	}
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < -1e9 || nums[i] > 1e9 {
+			fmt.Println("Number off the range")
+			os.Exit(0)
+		}
+		temp := nums[i]
+		if i < len(nums)-1 {
+			if nums[i+1] >= temp {
+				break
+			}
+		}
+	}
+
+	if target < -1e9 || target > 1e9 {
+		fmt.Println("target too low or high")
+		os.Exit(0)
+	}
+
+	var indices []int
+	for i, num := range nums {
+		if num == target {
+			fmt.Println(i)
+			indices = append(indices, i)
+		}
+	}
+	var ret_idx []int
+	ret_idx = append(ret_idx, indices[0])
+	ret_idx = append(ret_idx, indices[len(indices)-1])
+	return ret_idx
+
+}
+
+func main() {
+	// Example usage
+	array := []int{5, 7, 7, 7, 7, 7, 8, 8, 10}
+	target := 7
+
+	index := searchRange(array, target)
+	fmt.Print(index)
+}
