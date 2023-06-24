f1b4dfcfb0d65850164214f4cc71dcf76380fae2
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-24 09:33:46 +0530

    weekly contest350-special

--- specialPermutations.go
+++ specialPermutations.go
@@ -1,25 +1,27 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"os"
 )
 
 func specialPerm(nums []int) int {
-	
 
-	if len(nums) < 2 || len(nums) > 14{
+	if len(nums) < 2 || len(nums) > 14 {
 		fmt.Println("Array length not good")
+		os.Exit(0)
 	}
 
-	for num := range nums{
-		if nums[num] < 1 || nums[num] > 109{
-			fmt.Println("Numbers out of range"
+	for num := range nums {
+		if nums[num] < 1 || nums[num] > 109 {
+			fmt.Println("Numbers out of range")
+			os.Exit(0)
 		}
 	}
-    
+
+	return 0
 }
 
-func main(){
+func main() {
 
 }
