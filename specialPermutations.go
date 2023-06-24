f7190a910183351354f0b350b1a7a45854e82ec8
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-24 09:38:30 +0530

    permute functions added

--- specialPermutations.go
+++ specialPermutations.go
@@ -22,6 +22,32 @@ func specialPerm(nums []int) int {
 	return 0
 }
 
+func permute(nums []int) [][]int {
+	var result [][]int
+	backtrack(nums, 0, &result)
+	return result
+}
+
+func backtrack(nums []int, start int, result *[][]int) {
+	if start == len(nums) {
+		// Found a permutation
+		perm := make([]int, len(nums))
+		copy(perm, nums)
+		*result = append(*result, perm)
+	} else {
+		for i := start; i < len(nums); i++ {
+			// Swap current element with the start element
+			nums[start], nums[i] = nums[i], nums[start]
+
+			// Recursively permute the remaining elements
+			backtrack(nums, start+1, result)
+
+			// Restore the original order for backtracking
+			nums[start], nums[i] = nums[i], nums[start]
+		}
+	}
+}
+
 func main() {
 
 }
