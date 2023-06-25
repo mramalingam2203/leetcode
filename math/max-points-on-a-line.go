617e47992e1501a1574bc759be4e01a58ec8f306
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-25 16:15:16 +0530

    a hard problem as referred

---
+++ math/max-points-on-a-line.go
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/max-points-on-a-line/
+/*
+
+   Faster Sorting based Algorithm (More Efficient one)
+   Algorithm for Faster Sorting Based Algorithm
+
+   Initally consider a set of all points S={q1,q2,……qn}
+
+   1. Sort the points S. i.e S={q2,q1,q4…qn} arbitrary order
+   2. Lets take point q as origin (q ∈ S) … i.e (q=q2);
+   3. With respect to q find slope to {other points} in 2d plane.i.e(other points={S}-q)
+   4. Sort the all the points with the slope they make with respect to q
+   5. if there are more than 2 adjacent points together such that their slopes are equal with respect to q then the points adjacent are said to be collinear to the point q
+   6. if(!all points q are take as origin)
+       {
+           repeat from step 2 to step 6 by taking q = next q // i.e q=q1;
+       }else
+       {
+           exit(1);
+       }
+*/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func slope(points [][]int) float64 {
+	return (points[1][1] - points[0][1]) / (points[1][0] - points[0][0])
+}
+
+func maxPoints(points [][]int) int {
+	return 0
+}
+
+func main() {
+	points := make([][]int, 2)
+	for i := 0; i < n; i++ {
+    	points[i] = make([]int, 2)
+	points = {
+		{0, 1, 2, 3},
+		{4, 5, 6, 7},
+	}
+}
+
+}
