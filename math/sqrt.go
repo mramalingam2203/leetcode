24e35b1288812eb32e263980fa1efa375c9dc28e
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-25 12:43:02 +0530

    at last :-(

--- math/sqrt.go
+++ math/sqrt.go
@@ -5,20 +5,19 @@ package main
 import (
 	"fmt"
 	"math"
-	_ "os"
 	"strconv"
 )
 
 func babylonianGuess(x int) int {
+
 	runesArray := []rune(strconv.Itoa(x))
 	d := math.Ceil(float64(len(runesArray) / 2))
 	fmt.Println(d)
+
 	if d == 0 {
-		if x == 0 {
-			return 0
-		} else if x >= 1 || x < 4 {
+		if x >= 1 && x <= 3 {
 			return 1
-		} else if x > 4 || x <= 8 {
+		} else if x >= 4 && x < 9 {
 			return 2
 		} else if x == 9 {
 			return 3
@@ -36,16 +35,27 @@ func babylonianGuess(x int) int {
 
 }
 
+func smaller(a int, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+
+}
 func mySqrt(x int) int {
-	var epsilon float64 = 1e-7
+	if x == 0 {
+		return 0
+	}
+
+	var epsilon float64 = 1e-9
 	var result, result_prev int
 	result = babylonianGuess(x)
 	count := 0
 	for {
 		result_prev = result
 		result = (result_prev + x/result_prev) / 2
-		if math.Abs(float64(result-result_prev)) < epsilon {
-			break
+		if math.Abs(float64(result-result_prev)) < epsilon || count > 1e5 {
+			return smaller(result, result_prev)
 		}
 		count++
 		fmt.Println(count, result)
