572df74974b99057a8e459ae0d6cbd42f1810f48
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-25 08:45:44 +0530

    Plus 1 ing

---
+++ math/plus-one.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/plus-one/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func plusOne(digits []int) []int {
+
+}
+
+func main() {
+	a := []int{1, 2, 3}
+	plusOne(a)
+}
