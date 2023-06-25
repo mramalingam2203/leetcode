317154efe5b477b25f7f3d1fda418a8a419660a6
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-25 11:29:32 +0530

    algo wrongly implemented it seems

--- math/sqrt.go
+++ math/sqrt.go
@@ -12,11 +12,10 @@ import (
 func babylonianGuess(x int) int {
 	runesArray := []rune(strconv.Itoa(x))
 	d := math.Ceil(float64(len(runesArray) / 2))
-	fmt.Println(d)
 
 	guess2 := 2 * math.Pow(10, d-1)
 	guess7 := 7 * math.Pow(10, d-1)
-
+	fmt.Println(guess2, guess7)
 	if math.Abs(math.Pow(float64(guess2), 2)-float64(x)) < math.Abs(math.Pow(float64(guess7), 2)-float64(x)) {
 		return int(guess2)
 	} else {
@@ -26,10 +25,23 @@ func babylonianGuess(x int) int {
 }
 
 func mySqrt(x int) int {
-	return 0
+	var epsilon float64 = 1e-5
+	var result, result_prev int
+	result = babylonianGuess(x)
+
+	for {
+		result_prev = result
+		result = (result_prev + x/result_prev) / 2
+		if math.Abs(float64(result-result_prev)) > epsilon {
+			break
+		}
+	}
+
+	return result
+
 }
 
 func main() {
-	num := 20
-	fmt.Println(babylonianGuess(num))
+	num := 9981
+	fmt.Println(mySqrt(num))
 }
