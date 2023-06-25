7633107f52639910095b237d6f55525d93a3ffea
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-25 11:55:40 +0530

    great confusion with single digit numbers

--- math/sqrt.go
+++ math/sqrt.go
@@ -12,6 +12,18 @@ import (
 func babylonianGuess(x int) int {
 	runesArray := []rune(strconv.Itoa(x))
 	d := math.Ceil(float64(len(runesArray) / 2))
+	fmt.Println(d)
+	if d == 0 {
+		if x == 0 {
+			return 0
+		} else if x >= 1 || x < 4 {
+			return 1
+		} else if x > 4 || x <= 8 {
+			return 2
+		} else if x == 9 {
+			return 3
+		}
+	}
 
 	guess2 := 2 * math.Pow(10, d-1)
 	guess7 := 7 * math.Pow(10, d-1)
@@ -25,16 +37,19 @@ func babylonianGuess(x int) int {
 }
 
 func mySqrt(x int) int {
-	var epsilon float64 = 1e-10
+	var epsilon float64 = 1e-7
 	var result, result_prev int
 	result = babylonianGuess(x)
-
+	count := 0
 	for {
 		result_prev = result
 		result = (result_prev + x/result_prev) / 2
 		if math.Abs(float64(result-result_prev)) < epsilon {
 			break
 		}
+		count++
+		fmt.Println(count, result)
+
 	}
 
 	return result
@@ -42,6 +57,6 @@ func mySqrt(x int) int {
 }
 
 func main() {
-	num := 10020
+	num := 8
 	fmt.Println(mySqrt(num))
 }
