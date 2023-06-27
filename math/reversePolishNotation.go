c4313e7e716adaa282cd2d083b224ef72adbf46b
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 12:23:20 +0530

    added separate code for operand and operators

--- math/reversePolishNotation.go
+++ math/reversePolishNotation.go
@@ -43,22 +43,43 @@ func findOperand(char rune) bool {
 
 func findOperandI(s string) bool {
 	atoi, _ := strconv.Atoi(s)
-	if atoi < -200 && atoi > 200 {
+	if atoi < -200 || atoi > 200 {
 		fmt.Println("Out of range")
 		os.Exit(0)
 	}
+	fmt.Println(atoi)
 	return false
 
 }
 
-func main() {
-	//tokens = []string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}
-
-	runes := []rune("0123456789")
-	fmt.Println(runes)
-
-	for i := 0; i < len(runes); i++ {
-		fmt.Println(findOperand(runes[i]))
+func operate(x int, y int, operator rune) float64 {
+	if operator == 42 {
+		return float64(x * y)
+	} else if operator == 43 {
+		return float64(x / y)
+	} else if operator == 45 {
+		return float64(x + y)
+	} else if operator == 47 {
+		return float64(x - y)
 	}
+	return 0.0
+}
 
+func main() {
+	//tokens := []string{"10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"}
+	tokens := []string{"109", "-10"}
+	for i := 0; i < len(tokens); i++ {
+		runes := []rune(tokens[i])
+		if len(runes) == 1 {
+			fmt.Println(findOperator(runes[0]))
+		} else {
+			findOperandI(tokens[i])
+
+		}
+	}
+	/*
+		for i := 0; i < len(runes); i++ {
+			fmt.Println(findOperand(runes[i]))
+		}
+	*/
 }
