34d50d8b870970889e64e9fe69da9f38e29a7192
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 15:08:07 +0530

    weird algo

--- math/xl-sheet-column-title.go
+++ math/xl-sheet-column-title.go
@@ -13,10 +13,23 @@ fmt(
 func convertToTitle(columnNumber int) string {
 	var s string
 	var i, rem int 
-	i = 0
 
 	for ;;;{
 		rem = columnNumber%26
+		if rem == 0 {
+			s = s(append, "Z")
+			columnNumber = (columnNumber/26)-1
+		}else{
+			s = s(append, strconv.Itoa(rem-1) + "A")
+			columnNumber /= 26
+		}
 	}
+
+	fmt.Print(s)
     
+}
+
+
+func main(){
+	convertToTitle(23)
 }
