084e810b74be53d779df942a43a0a115d8fa2395
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 17:12:29 +0530

    improved

--- math/perfect-squares.go
+++ math/perfect-squares.go
@@ -7,23 +7,33 @@ import (
 )
 
 func numSquares(n int) int {
-	if math.Sqrt(n) - math.Floor(math.Sqrt(n)) == 0 {
-		return 1}
-	if n <= 3{
+	if math.Sqrt(n)-math.Floor(math.Sqrt(n)) == 0 {
+		return 1
+	}
+	if n <= 3 {
 		return n
 	}
 	res := n
 	var temp int
-	for x:=1; x <= n; x++ {
-		temp = x*x 
-		if temp > n{
+	for x := 1; x <= n; x++ {
+		temp = x * x
+		if temp > n {
 			break
-		}else{
-			res = min(res, 1 + numSquares(n-temp))
+		} else {
+			res = min(res, 1+numSquares(n-temp))
 		}
-		return res
 	}
-	
+
+	return res
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
 
 func main() {
 	fmt.Println(numSquares(20))
+}
