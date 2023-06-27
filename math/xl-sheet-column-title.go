6c1408a7fc01f8b8feaa6352bde9ea771396adcf
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 16:57:48 +0530

    very jumbled now :(

--- math/xl-sheet-column-title.go
+++ math/xl-sheet-column-title.go
@@ -4,9 +4,10 @@ package main
 
 import (
 	"fmt"
-	"os"
+	_ "os"
 )
 
+/*
 func convertToTitle(columnNumber int) string {
 	var s string
 	var rem int
@@ -16,9 +17,10 @@ func convertToTitle(columnNumber int) string {
 		if rem == 0 {
 			//	s = s + "Z")
 			columnNumber = (columnNumber / 26) - 1
+			fmt.Println(columnNumber)
 		} else {
 			//s = s+ strconv.Itoa(rem-1)+"A")
-			columnNumber /= 26"
+			columnNumber /= 26
 		}
 	}
 
@@ -26,7 +28,39 @@ func convertToTitle(columnNumber int) string {
 	return "hi"
 
 }
+*/
+
+func convertToTitle(num int) string {
+	var alpha string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var res string = ""
+
+	if num < 26 {
+		res = res +
+		fmt.Println(alpha[num-1])
+		return res
+	} else{
+		q := (num / 26)
+		r := num % 26
+		res := ""
+
+		if(r == 0){
+		  if(q == 1){
+			res.convertToTitle(1,alpha[(26 + r-1)%26]);
+		  }
+		  else{
+			res = num_hash(q-1);
+			res.append(1,alpha[(26 + r-1)%26]);
+		  }
+		}
+		else{
+		  res = num_hash(q);
+		  res.append(1,alpha[(26 + r-1)%26]);
+		}
+		return res;
+	  }
+	
+}
 
 func main() {
-	//	convertToTitle(23)
+	fmt.Print(convertToTitle(230))
 }
