001bb39f9afd7e58da9e555cf6450f7d3c36e7a7
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 15:02:10 +0530

    begin xl sheet column title

---
+++ math/xl-sheet-column-title.go
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/excel-sheet-column-title
+
+
+package main
+
+fmt(
+	"os"
+	"fmt"
+)
+
+
+
+func convertToTitle(columnNumber int) string {
+	var s string
+	var i, rem int 
+	i = 0
+
+	for ;;;{
+		rem = columnNumber%26
+	}
+    
+}
