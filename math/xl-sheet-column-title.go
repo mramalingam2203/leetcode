cc87b0c20b01304bf8d24a7b8baef6aa15a4dc02
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-27 15:13:05 +0530

    main algo coded

--- math/xl-sheet-column-title.go
+++ math/xl-sheet-column-title.go
@@ -1,35 +1,32 @@
 // https://leetcode.com/problems/excel-sheet-column-title
 
-
 package main
 
-fmt(
-	"os"
+import (
 	"fmt"
+	"os"
 )
-
-
 
 func convertToTitle(columnNumber int) string {
 	var s string
-	var i, rem int 
+	var rem int
 
-	for ;;;{
-		rem = columnNumber%26
+	for {
+		rem = columnNumber % 26
 		if rem == 0 {
-			s = s(append, "Z")
-			columnNumber = (columnNumber/26)-1
-		}else{
-			s = s(append, strconv.Itoa(rem-1) + "A")
-			columnNumber /= 26
+			//	s = s + "Z")
+			columnNumber = (columnNumber / 26) - 1
+		} else {
+			//s = s+ strconv.Itoa(rem-1)+"A")
+			columnNumber /= 26"
 		}
 	}
 
 	fmt.Print(s)
-    
+	return "hi"
+
 }
 
-
-func main(){
-	convertToTitle(23)
+func main() {
+	//	convertToTitle(23)
 }
