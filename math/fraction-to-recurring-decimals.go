993f222d046d98bea386e9a2d1d8651327fabe41
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 10:24:14 +0530

    added code for floa division

--- math/fraction-to-recurring-decimals.go
+++ math/fraction-to-recurring-decimals.go
@@ -2,20 +2,31 @@
 
 package main
 
-import(
-	_ "fmt"
-	_ "os"
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
 )
 
 func fractionToDecimal(numerator int, denominator int) string {
-	if denominator == 0 || numerator < int(math.Pow(2.0, -31)) ||  denominator < int(math.Pow(2.0, -31))  ||  numerator > int(math.Pow(2.0, 31))  ||  denominator > int(math.Pow(2.0, 31)) {
+	if denominator == 0 || numerator < int(math.Pow(2.0, -31)) || denominator < int(math.Pow(2.0, -31)) || numerator > int(math.Pow(2.0, 31)) || denominator > int(math.Pow(2.0, 31)) {
 		fmt.Println("Numbers out of range")
 		os.Exit(0)
 	}
-    
+
+	if numerator%denominator == 0 {
+		return strconv.Itoa(numerator / denominator)
+	} else {
+		return fmt.Sprintf("%f", float64(numerator)/float64(denominator))
+
+	}
+
+	return "hi"
 }
 
-func main)){
-
-	fractionToDecimal(numerator int, denominator int)
+func main() {
+	num := 1
+	den := 3
+	fmt.Println(fractionToDecimal(num, den))
 }
