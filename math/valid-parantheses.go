a105ae282d306ab0754e6dabc32693cee488a64f
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 08:58:06 +0530

    corrected

--- math/valid-parantheses.go
+++ math/valid-parantheses.go
@@ -13,22 +13,6 @@ func isValid(s string) bool {
 		os.Exit(0)
 	}
 
-	srune := []rune(s)
-	// bracket (lr) -- 40, 41
-	// bracket (lr) -- 91, 93
-	// brace (lr) -- 123, 125
-	fmt.Println(srune)
-
-	/*
-		for i := 0; i < len(srune); i++ {
-			if srune[i] != 40 || srune[i] != 41 || srune[i] != 91 || srune[i] != 93 || srune[i] != 123 || srune[i] != 125 {
-				fmt.Println(srune[i])
-				fmt.Println("Invalid character in string")
-				os.Exit(0)
-			}
-		}
-	*/
-
 	stack := []rune{}
 	bracketsMap := map[rune]rune{
 		')': '(',
