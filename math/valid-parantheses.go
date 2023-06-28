43adb1af09a954a7e19c8a198e8511960c01d4e3
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 08:49:00 +0530

    using stack map

--- math/valid-parantheses.go
+++ math/valid-parantheses.go
@@ -7,30 +7,6 @@ import (
 	"os"
 )
 
-type Stack []string
-
-// IsEmpty: check if stack is empty
-func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
-}
-
-// Push a new value onto the stack
-func (s *Stack) Push(str string) {
-	*s = append(*s, str) // Simply append the new value to the end of the stack
-}
-
-// Remove and return top element of stack. Return false if stack is empty.
-func (s *Stack) Pop() (string, bool) {
-	if s.IsEmpty() {
-		return "", false
-	} else {
-		index := len(*s) - 1   // Get the index of the top most element.
-		element := (*s)[index] // Index into the slice and obtain the element.
-		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
-		return element, true
-	}
-}
-
 func isValid(s string) bool {
 	if len(s) < 1 || len(s) > 1e4 {
 		fmt.Println("Invalid length of string")
@@ -53,11 +29,28 @@ func isValid(s string) bool {
 		}
 	*/
 
-	var stack Stack
+	stack := []rune{}
+	bracketsMap := map[rune]rune{
+		')': '(',
+		'}': '{',
+		']': '[',
+	}
 
+	for _, char := range s {
+		if char == '(' || char == '{' || char == '[' {
+			stack = append(stack, char)
+		} else if char == ')' || char == '}' || char == ']' {
+			if len(stack) == 0 || stack[len(stack)-1] != bracketsMap[char] {
+				return false
+			}
+			stack = stack[:len(stack)-1] // Pop the last element
+		}
+	}
+
+	return true
 }
 
 func main() {
-	fmt.Println(isValid("(){}[]"))
+	fmt.Println(isValid("(){}[}"))
 
 }
