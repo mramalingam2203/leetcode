90842968bdbc5b2954477c2328b13b09efde2dc0
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-28 08:46:07 +0530

    added Stack structure

--- math/valid-parantheses.go
+++ math/valid-parantheses.go
@@ -7,6 +7,30 @@ import (
 	"os"
 )
 
+type Stack []string
+
+// IsEmpty: check if stack is empty
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+// Push a new value onto the stack
+func (s *Stack) Push(str string) {
+	*s = append(*s, str) // Simply append the new value to the end of the stack
+}
+
+// Remove and return top element of stack. Return false if stack is empty.
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		index := len(*s) - 1   // Get the index of the top most element.
+		element := (*s)[index] // Index into the slice and obtain the element.
+		*s = (*s)[:index]      // Remove it from the stack by slicing it off.
+		return element, true
+	}
+}
+
 func isValid(s string) bool {
 	if len(s) < 1 || len(s) > 1e4 {
 		fmt.Println("Invalid length of string")
@@ -29,15 +53,8 @@ func isValid(s string) bool {
 		}
 	*/
 
-	for i := 0; i < len(srune)-1; i += 2 {
-		fmt.Println(srune[i])
-		if srune[i] != srune[i+1] {
-			continue
-		}
-		return true
-	}
+	var stack Stack
 
-	return false
 }
 
 func main() {
