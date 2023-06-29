02e53cb4572994fb7ca3ac5feb4aff9ddd3a343d
mramalingam2203 <dr.muthukumaran@live.com>
2023-06-29 11:14:48 +0530

    add rune manipulation

--- valid-anagram.go
+++ valid-anagram.go
@@ -5,30 +5,39 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 	"unicode"
 )
 
 func isAnagram(s string, t string) bool {
-	if len(s) < 1 || len(t) < 1 || len(s) > 5e4 || len(t) > 5e4{
+	if len(s) < 1 || len(t) < 1 || len(s) > 5e4 || len(t) > 5e4 {
 		fmt.Println("Strign lengths invalid")
 		os.Exit(0)
 	}
 
 	if len(s) != len(t) {
 		return false
-		}
-		
+	}
+	runeS := []rune(s)
+	runeT := []rune(t)
 
-	for i := range s{
-		if !unicode.IsLetter(s[i]){
+	for i := range runeS {
+		if !unicode.IsLetter(runeS[i]) {
 			return false
 		}
 	}
+
+	for i := range runeT {
+		if !unicode.IsLetter(runeT[i]) {
+			return false
+		}
+	}
+
+	return true
 }
 
-
-func main{
-	isAnagram("hello world", "world ellohe")
+func main() {
+	s1 := "hello world"
+	s2 := "world ellohe"
+	fmt.Println(isAnagram(s1, s2))
 
 }
