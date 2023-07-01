6a4bd5afa34344db4c271addb285824c3da0f37c
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-01 11:51:59 +0530

    roundabout algo

--- repeated-string-pattern.go
+++ repeated-string-pattern.go
@@ -13,6 +13,30 @@ func main() {
 
 func repeatedSubstringPattern(s string) bool {
 	runeS := []rune(s)
-	fmt.Println(runeS)
+
+	runes_2D := make([][]rune, 3)
+	runes_2D = convertTo2D(runeS, 3)
+	fmt.Print(runes_2D)
+
 	return true
+}
+
+func convertTo2D(slice []rune, columns int) [][]rune {
+	length := len(slice)
+	rows := length / columns
+	if length%columns != 0 {
+		rows++
+	}
+
+	result := make([][]rune, rows)
+	for i := 0; i < rows; i++ {
+		start := i * columns
+		end := start + columns
+		if end > length {
+			end = length
+		}
+		result[i] = slice[start:end]
+	}
+
+	return result
 }
