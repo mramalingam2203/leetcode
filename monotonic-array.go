92f859065ee58027cc679f89bfadc9987c340cae
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-01 13:36:51 +0530

    prev approach wrong

--- monotonic-array.go
+++ monotonic-array.go
@@ -2,19 +2,40 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	nums := []int{1, 2, 3, 4}
-	isMonotonic(nums)
+	nums := []int{1, 2, 2, 3}
+	fmt.Println(isMonotonic(nums))
 }
 
 func isMonotonic(nums []int) bool {
-	temp := nums[1] - nums[0]
-	for i := 1; i < len(nums)-1; i++ {
-		if nums[i+1]-nums[i] != temp {
-			return false
-		}
-		temp = nums[i+1] - nums[i]
+
+	//constraints
+	if len(nums) < 1 || len(nums) > 1e5 {
+		fmt.Println("integer array length out of range")
+		os.Exit(0)
 	}
 
-	return true
+	for i := range nums {
+		if nums[i] < -1e5 || nums[i] > 1e5 {
+			fmt.Println("integer array elements invalid")
+			os.Exit(0)
+		}
+	}
+
+	var result bool
+
+	for i := 0; i < nums.Len(); i++ {
+		if nums[i] < nums[i+1] {
+			result &= true
+		} else {
+			result &= false
+		}
+	}
+
+	return result
 }
