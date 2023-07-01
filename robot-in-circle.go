d5772333c2f4eca20a4844f43db2da49e9fad60e
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-01 18:53:25 +0530

    check robot moves

---
+++ robot-in-circle.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/contest/warm-up-contest/problems/lexicographical-numbers/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	errands := "GGLLRRG"
+	isRobotBounded(errands)
+}
+
+func isRobotBounded(instructions string) bool {
+
+}
+
+func move([]string) []int {
+
+}
