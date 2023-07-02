97f835d53898b3d359b3f9613e7a82cdda780a72
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-02 11:41:37 +0530

    constraints not added but leetcode accepts

--- diagonal-sum-of-matrix.go
+++ diagonal-sum-of-matrix.go
@@ -2,14 +2,36 @@ package main
 
 import (
 	"fmt"
-	"os"
+	//"os"
 )
 
 func main() {
-	matrix := [][]int{{1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}, {1, 2, 3, 4}}
+	matrix := [][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}
+	fmt.Println(diagonalSum(matrix))
 
 }
 
 func diagonalSum(mat [][]int) int {
-
+	rows := len(mat)
+	columns := len(mat)
+	sum := 0
+	for i := 0; i < rows; i++ {
+		for j := 0; j < columns; j++ {
+			if i == j || i == rows-(j+1) {
+				sum += mat[i][j]
+			}
+		}
+	}
+	/*
+		for i := 0; i < rows; i++ {
+			for j := 0; j < columns; j++ {
+				if i == rows-(j+1) {
+					//fmt.Println(i, j)
+					fmt.Println(mat[i][j])
+					sum += mat[i][j]
+				}
+			}
+		}
+	*/
+	return sum
 }
