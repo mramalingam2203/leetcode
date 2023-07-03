a7dade1b92457f5d14246cdd1148917bf5f367ce
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-03 12:38:31 +0530

    init code

---
+++ contests/prime-number-pair.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/contest/weekly-contest-352/problems/prime-pairs-with-target-sum/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+}
+
+func findPrimePairs(n int) [][]int {
+	if n < 1 || n < 1e6 {
+		fmt.Println("Numbers out of range")
+		os.Exit(0)
+	}
+
+}
