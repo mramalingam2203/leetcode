9b68408eb57ba3ed47a17fc08d28087a6f3abeda
mramalingam2203 <dr.muthukumaran@live.com>
2023-07-03 09:28:36 +0530

    quite difficult

---
+++ contests/Longest-Even-Odd-Subarray-With-Threshold.go
@@ -0,0 +1,35 @@
+// https://leetcode.com/contest/weekly-contest-352/problems/longest-even-odd-subarray-with-threshold/
+
+package main
+
+import (
+	//"os"
+	"fmt"
+)
+
+func main() {
+	array := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(findSubarrays(array))
+	for i := range array {
+		longestAlternatingSubarray(array[i], )
+}
+
+
+func longestAlternatingSubarray(nums []int, threshold int) int {
+    
+}
+
+
+func findSubarrays(arr []int) [][]int {
+	n := len(arr)
+	subarrays := [][]int{}
+
+	for i := 0; i < n; i++ {
+		for j := i; j < n; j++ {
+			subarray := arr[i : j+1]
+			subarrays = append(subarrays, subarray)
+		}
+	}
+
+	return subarrays
+}
