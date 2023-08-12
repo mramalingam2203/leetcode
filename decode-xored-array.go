e4fd249c68d85fe98d0d6e2b4b8c24d3f6a20d46
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-12 11:59:45 +0530

    decode an XORed array

---
+++ decode-xored-array.go
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/decode-xored-array/
+
+package main
+
+import "math"
+
+func decode(encoded []int, first int) []int {
+
+	key := math.Pow(float64(encoded[0]), float64(first))
+
+}
+
+func main() {
+	ncoded = []int{6, 2, 7, 3}
+	start = 4
+
+	decode(ncoded, start)
+}
