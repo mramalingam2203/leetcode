d82bc038aa93f9f6f8eaaad052c91336377782cb
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-12 18:32:59 +0530

    works locally

--- find-greatest-common-divisor-of-array.go
+++ find-greatest-common-divisor-of-array.go
@@ -2,26 +2,28 @@
 
 package main
 
+import (
+	"fmt"
+	"sort"
+)
 
 func findGCD(nums []int) int {
 
 	sort.Ints(nums)
+	fmt.Println(nums)
+	return gcd(nums[0], nums[len(nums)-1])
 
-    
 }
 
-
-func gcd(a, b)int{
-    for b!=0 {
-        a, b = b, a % b
+func gcd(a int, b int) int {
+	for b != 0 {
+		a, b = b, a%b
 	}
-    return a
+	return a
 
 }
 
-
-
-func main({
-	gcd(5, 10)
-	list := []int{2,5, 6, 9, 10}
-})
+func main() {
+	list := []int{7, 5, 6, 8, 3}
+	fmt.Println(findGCD(list))
+}
