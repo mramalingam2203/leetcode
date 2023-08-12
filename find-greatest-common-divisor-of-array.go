7e5f624a11ac747ca08974b14657c60ecc4e9516
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-12 18:28:26 +0530

    init code

---
+++ find-greatest-common-divisor-of-array.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/find-greatest-common-divisor-of-array/
+
+package main
+
+
+func findGCD(nums []int) int {
+
+	sort.Ints(nums)
+
+    
+}
+
+
+func gcd(a, b)int{
+    for b!=0 {
+        a, b = b, a % b
+	}
+    return a
+
+}
+
+
+
+func main({
+	gcd(5, 10)
+	list := []int{2,5, 6, 9, 10}
+})
