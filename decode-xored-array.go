76eb8d4cef6f44e3059412a2fe788e94b33bebd4
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-12 12:13:48 +0530

    several confusions

--- decode-xored-array.go
+++ decode-xored-array.go
@@ -2,17 +2,22 @@
 
 package main
 
-import "math"
+import (
+	"fmt"
+)
 
 func decode(encoded []int, first int) []int {
+	key := encoded[0] ^ first
 
-	key := math.Pow(float64(encoded[0]), float64(first))
-
+	for i := 0; i < len(encoded)-1; i++ {
+		fmt.Println(key ^ encoded[i+1])
+	}
+	return encoded
 }
 
 func main() {
-	ncoded = []int{6, 2, 7, 3}
-	start = 4
+	ncoded := []int{1, 2, 3}
+	start := 1
 
-	decode(ncoded, start)
+	fmt.Println(decode(ncoded, start))
 }
