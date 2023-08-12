19043b9d9afd618dff96570a212cd5a4997378ae
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-12 10:55:50 +0530

    left right sum in 1d array

---
+++ left-and-right-sum-difference.go
@@ -0,0 +1,42 @@
+// https://leetcode.com/problems/running-sum-of-1d-array/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	list := []int{3, 1, 2, 10, 1}
+	fmt.Println(runningSum(list))
+}
+
+func leftRightDifference(nums []int) []int {
+	if len(nums) < 1 || len(nums) > 1000 {
+		fmt.Println("invalid inputs")
+		os.Exit(0)
+	}
+
+	fmt.Println(cumulativeSum(nums))
+
+}
+
+func cumulativeSum(nums []int) []int {
+	sums := make([]int, len(nums))
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] < 1 || nums[i] > 1e5 {
+			fmt.Println("invalid inputs")
+			os.Exit(0)
+		}
+
+		if i != 0 {
+			sums[i] = sums[i-1] + nums[i]
+		} else {
+			sums[i] = nums[i]
+		}
+	}
+
+	return sums
+}
