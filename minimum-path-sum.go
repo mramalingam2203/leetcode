58e79a640921ad08db12d879beb6dd1223f8d526
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-13 11:39:18 +0530

    code fully rounded

---
+++ minimum-path-sum.go
@@ -0,0 +1,48 @@
+// https://leetcode.com/problems/minimum-path-sum/
+
+package main
+
+import "fmt"
+
+func minPathSum(grid [][]int) int {
+
+	m := len(grid)
+	n := len(grid[0])
+
+	// Create a DP table of the same size as the grid
+	// Define the size of the 2D slice
+
+	dp := make([][]int, m)
+	for i := range dp {
+		dp[i] = make([]int, n)
+	}
+
+	// Initialize the DP table with the value of the first cell
+	dp[0][0] = grid[0][0]
+
+	// Fill in the DP table
+	for i := 1; i <= m-1; i++ {
+		dp[i][0] = dp[i-1][0] + grid[i][0]
+	}
+
+	for j := 1; j <= n-1; j++ {
+		dp[0][j] = dp[0][j-1] + grid[0][j]
+	}
+
+	for i := 1; i <= m-1; i++ {
+		for j := 1; j <= n-1; j++ {
+			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
+		}
+	}
+
+	return dp[m-1][n-1]
+
+}
+
+func main() {
+
+	mesh := [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}
+
+	fmt.Println(minPathSum(mesh))
+
+}
