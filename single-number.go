9d25d2f0da666be75d0f19bf9b75da8d7c27772e
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-13 12:58:33 +0530

    init code

---
+++ single-number.go
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/single-number/
+
+package main
+
+func singleNumber(nums []int) int {
+
+}
+
+func main() {
+
+	array := []int{2, 2, 1}
+
+	singleNumber(array)
+
+}
