112d63f5bf465ac6046ffcda9639478e2d50ec53
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-13 11:43:39 +0530

    added constraints accepted by leetcode

--- minimum-path-sum.go
+++ minimum-path-sum.go
@@ -9,6 +9,10 @@ func minPathSum(grid [][]int) int {
 	m := len(grid)
 	n := len(grid[0])
 
+	if m < 1 || m > 200 || n < 1 || n > 200 {
+		return 0
+	}
+
 	// Create a DP table of the same size as the grid
 	// Define the size of the 2D slice
 
@@ -22,6 +26,7 @@ func minPathSum(grid [][]int) int {
 
 	// Fill in the DP table
 	for i := 1; i <= m-1; i++ {
+
 		dp[i][0] = dp[i-1][0] + grid[i][0]
 	}
 
@@ -31,6 +36,11 @@ func minPathSum(grid [][]int) int {
 
 	for i := 1; i <= m-1; i++ {
 		for j := 1; j <= n-1; j++ {
+
+			if grid[i][j] < 0 || grid[i][j] > 200 {
+				return 0
+			}
+
 			dp[i][j] = min(dp[i-1][j], dp[i][j-1]) + grid[i][j]
 		}
 	}
@@ -39,9 +49,17 @@ func minPathSum(grid [][]int) int {
 
 }
 
+func min(a int, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
+}
+
 func main() {
 
-	mesh := [][]int{{1, 3, 1}, {1, 5, 1}, {4, 2, 1}}
+	mesh := [][]int{{1, 2, 3}, {4, 5, 6}}
 
 	fmt.Println(minPathSum(mesh))
 
