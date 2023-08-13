f06feb9178fd118d79f34bcb7cbd29b7cd63e361
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-13 13:06:04 +0530

    leetcode accepted

--- single-number.go
+++ single-number.go
@@ -2,14 +2,38 @@
 
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 func singleNumber(nums []int) int {
+
+	if len(nums) < 1 || len(nums) > 3e4 {
+		return 0
+	}
+
+	sort.Ints(nums)
+
+	fmt.Println(nums)
+
+	for i := 0; i < len(nums)-1; i += 2 {
+		if nums[i] < -3e4 || nums[i] > 3e4 {
+			return 0
+		}
+
+		if nums[i] != nums[i+1] {
+			return nums[i]
+		}
+	}
+	return nums[len(nums)-1]
 
 }
 
 func main() {
 
-	array := []int{2, 2, 1}
+	array := []int{2, 2, 1, 1, 3, 3, 2, 4, 3, 3}
 
-	singleNumber(array)
+	fmt.Println(singleNumber(array))
 
 }
