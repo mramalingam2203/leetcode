f145e04efd2d470e7c57d3a91b391174585f9923
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-14 11:25:27 +0530

    earlier didnt serve the purpose

--- largest-number.go
+++ largest-number.go
@@ -3,10 +3,7 @@
 package main
 
 import (
-	"fmt"
-	"sort"
 	"strconv"
-	"strings"
 )
 
 func largestNumber(nums []int) string {
@@ -24,20 +21,6 @@ func largestNumber(nums []int) string {
 		s1[i] = strconv.Itoa(nums[i])
 	}
 
-	s2 := []string{}
-
-	for i := 0; i < len(s1); i++ {
-		for j := 0; j < len(s1[i]); j++ {
-			s2 = append(s2, string(s1[i][j]))
-		}
-	}
-
-	sort.Sort(sort.Reverse(sort.StringSlice(s2)))
-
-	s3 := strings.Join(s2, "")
-	fmt.Println(s3)
-
-	return s3
 }
 
 func main() {
