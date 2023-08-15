6b4e048a76e103011607031423bbdf466cdd4214
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-15 08:34:00 +0530

    the recurring fraction

---
+++ fraction-to-recurring-decimal.go
@@ -0,0 +1,58 @@
+//https: //leetcode.com/problems/fraction-to-recurring-decimal/
+
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func fractionToDecimal(numerator int, denominator int) string {
+
+	if numerator == 0 {
+		return "0"
+	}
+
+	result := ""
+
+	// Determine the sign of the result
+	if numerator < 0 && denominator > 0 || numerator > 0 && denominator < 0 {
+		result += "-"
+	}
+
+	// Calculate integer part
+	result += strconv.Itoa(numerator / denominator)
+
+	remainder := numerator % denominator
+	if remainder == 0 {
+		return result
+	}
+
+	result += "."
+
+	for remainder != 0 {
+
+		remainder *= 10
+		result += strconv.Itoa(remainder / denominator)
+		remainder %= denominator
+
+		fmt.Println(remainder)
+	}
+
+	return result
+}
+
+func abs(a int) int {
+
+	if a < 0 {
+		return -1 * a
+	}
+
+	return a
+
+}
+
+func main() {
+
+	fractionToDecimal(22, 7)
+}
