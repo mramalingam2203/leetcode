3225e165bc3b80c2da15107a9b8d04aaf77a65e2
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 08:00:35 +0530

    add init code

---
+++ maximum-product-subarray.go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/maximum-product-subarray/
+
+package main
+
+func main() {
+	array := []int{1, 2, 3, 4}
+	generateSubArrays(array)
+}
+
+func maxProduct(nums []int) int {
+
+	if len(nums) < 1 || len(nums) > 2e4 {
+		return 0
+	}
+
+}
+
+func generateSubArrays(nums []int) [][]int {
+
+	subarray := make([][]int)
+
+	return subarrays
+
+}
