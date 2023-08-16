26324289f555ef3a5796f9a914106cc96e66af33
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 12:28:35 +0530

    code works local

--- factorial-trailing-zeroes.go
+++ factorial-trailing-zeroes.go
@@ -2,10 +2,38 @@
 
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
-	trailingZeroes(10)
+	fmt.Println(trailingZeroes(0))
 }
 
 func trailingZeroes(n int) int {
 
+	if n < 0 || n > 1e4 {
+		return 0
+	}
+	fact := factorial(n)
+	s := strconv.Itoa(fact)
+	count := 0
+
+	for i := 0; i < len(s); i++ {
+		if string(s[i]) == "0" {
+			count++
+		}
+
+	}
+	return count
+
+}
+
+func factorial(n int) int {
+	if n == 0 || n == 1 {
+		return 1
+	}
+
+	return n * factorial(n-1)
 }
