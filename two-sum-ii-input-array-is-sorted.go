910e726b8c3bd2516eab8f0704f47335db14d6a6
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 13:32:30 +0530

    works local

--- two-sum-ii-input-array-is-sorted.go
+++ two-sum-ii-input-array-is-sorted.go
@@ -2,23 +2,37 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
 
-	array := []int{2, 7, 11, 15}
-	number := 9
+	array := []int{2, 3, 4}
+	number := 6
+	fmt.Println(twoSum(array, number))
 
 }
 
 func twoSum(numbers []int, target int) []int {
 
+	if len(numbers) < 2 || len(numbers) > 3e4 || target < -1000 || target > 1000 {
+		fmt.Println("lenghts invalid")
+		os.Exit(0)
+	}
+	indices := []int{}
 	for i := 0; i < len(numbers); i++ {
 		for j := i; j < len(numbers); j++ {
-			fmt.Println(nums[i] + nums[j])
+			if numbers[i]+numbers[j] == target {
+				if i != j {
+					indices = append(indices, i+1)
+					indices = append(indices, j+1)
+					return indices
+				}
+			}
 		}
 	}
-
-	return nums
+	return indices
 
 }
