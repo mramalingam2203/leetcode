92913a8a0dbcedd28d41d0ee09c6f6139573338a
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 13:21:49 +0530

    init code

---
+++ two-sum-ii-input-array-is-sorted.go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+package main
+
+import "fmt"
+
+func main() {
+
+	array := []int{2, 7, 11, 15}
+	number := 9
+
+}
+
+func twoSum(numbers []int, target int) []int {
+
+	for i := 0; i < len(numbers); i++ {
+		for j := i; j < len(numbers); j++ {
+			fmt.Println(nums[i] + nums[j])
+		}
+	}
+
+	return nums
+
+}
