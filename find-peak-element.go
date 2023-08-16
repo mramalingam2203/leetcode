85e392746ff90522bfb9c15c5f2dc8446beb47ed
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 09:37:26 +0530

    added init code

---
+++ find-peak-element.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-peak-element/
+
+package main
+
+func main() {
+	array := []int{[1,2,3,1]}
+	findPeakElement(array)
+}
+
+func findPeakElement(nums []int) int {
+
+	n := len(nums)
+	
+
+
+
+}
