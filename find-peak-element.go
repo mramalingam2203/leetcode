f233d933516967fa03c3839442e599eee417b312
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-16 09:55:40 +0530

    works locally but fails on leetcode

--- find-peak-element.go
+++ find-peak-element.go
@@ -2,16 +2,46 @@
 
 package main
 
+import "fmt"
+
 func main() {
-	array := []int{[1,2,3,1]}
-	findPeakElement(array)
+	array := []int{1, 2, 3, 4}
+	//findPeakElement(array)
+	fmt.Println(max(array))
 }
 
 func findPeakElement(nums []int) int {
 
 	n := len(nums)
-	
+	if n < 1 || n > 1000 {
+		return 0
+	}
+	var peak int
 
+	for i := 1; i < n-1; i++ {
 
+		if nums[i] > nums[i-1] && nums[i] > nums[i+1] {
+			peak = i
+		}
+
+	}
+
+	if n <= 3 {
+		peak = max(nums)
+	}
+
+	return peak
+
+}
+
+func max(array []int) int {
+	biggest := 0
+	for i := 0; i < len(array); i++ {
+		if array[i] > array[biggest] {
+			biggest = i
+		}
+	}
+
+	return biggest
 
 }
