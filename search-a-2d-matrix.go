65c703b765f6712008e602fdb1f9010fb470f016
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 13:25:24 +0530

    constraints code not written

--- search-a-2d-matrix.go
+++ search-a-2d-matrix.go
@@ -1,37 +1,37 @@
-https://leetcode.com/problems/search-a-2d-matrix/
+//https://leetcode.com/problems/search-a-2d-matrix/
 
 package main
 
+import "fmt"
 
 func main() {
 
-	array := [][]int{{1,3,5,7},{10,11,16,20},{23,30,34,60}}
-	searchMatrix(array)
+	array := [][]int{{1, 3, 5, 7}, {10, 11, 16, 20}, {23, 30, 34, 60}}
+	target := 11
+	fmt.Println(searchMatrix(array, target))
 }
 
 func searchMatrix(matrix [][]int, target int) bool {
 
 	m := len(matrix)
-    n := len(matrix[0])
+	n := len(matrix[0])
 
-    left := 0
-    right := m * n - 1
+	left := 0
+	right := m*n - 1
 
 	for left <= right {
 
-		mid := left + (right - left) / 2
-		midValue := matrix[mid / n][mid % n]
+		mid := left + (right-left)/2
+		midValue := matrix[mid/n][mid%n]
 
-		if midValue == target{
+		if midValue == target {
 			return true
-		} else if midValue < target{
-			left = mid++
-		}else{
-			right = mid--
+		} else if midValue < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
 		}
 	}
 
-	return bool
-
-    
+	return false
 }
