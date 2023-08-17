42374317dbec6fb577c40bb01ff6700ccac792b3
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 09:28:59 +0530

    init code

---
+++ search-in-rotated-sorted-array-ii.go
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+
+package main
+
+func search(nums []int, target int) bool {
+
+	if len(nums) < 1 || len(nums) > 5000 || target < -1e4 || target > 1e4 {
+		return false
+	}
+
+}
+
+func findPivotIndex(nums []int) int {
+	left := 0
+	right := length(nums) - 1
+
+}
+func main() {
+
+}
