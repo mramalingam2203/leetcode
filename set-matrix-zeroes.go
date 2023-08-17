98e23f100911aa52dbb5edc20b02f4ea4cacbe09
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 11:30:10 +0530

    initial code add

---
+++ set-matrix-zeroes.go
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/set-matrix-zeroes/
+
+package main
+
+func main() {
+
+}
+
+func setZeroes(matrix [][]int) {
+
+	rows = len(matrix)
+	cols = len(matrix[0])
+
+	zeroRows := make(map[int]int)
+	zeroCols := make(map[int]int)
+
+}
