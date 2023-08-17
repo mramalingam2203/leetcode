ed2e0eac384163264b9b5c7fb4d8724ac8abac5a
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 10:53:50 +0530

    added constraints works locally and leecode accepted

--- climbing-stairs.go
+++ climbing-stairs.go
@@ -5,15 +5,19 @@ package main
 import "fmt"
 
 func main() {
-	n := 2
+	n := 3
 	fmt.Println(climbStairs(n))
 
 }
 
 func climbStairs(n int) int {
 
-	if n <= 2 {
+	if n < 1 || n > 45 {
 		return 0
+	}
+
+	if n <= 2 {
+		return n
 	}
 
 	ways := make([]int, n+1)
@@ -22,10 +26,10 @@ func climbStairs(n int) int {
 	ways[1] = 2
 
 	// Calculate the number of ways for each step using dynamic programming
-	for i := 2; i <= n; i++ {
+	for i := 2; i < n; i++ {
 		ways[i] = ways[i-1] + ways[i-2]
 	}
 
-	return ways[n]
+	return ways[n-1]
 
 }
