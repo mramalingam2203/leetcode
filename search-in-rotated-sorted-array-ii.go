4c799ee1be7edc3a4541c90e0deef901e0e8d7f1
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 09:36:45 +0530

    find pivot index code added

--- search-in-rotated-sorted-array-ii.go
+++ search-in-rotated-sorted-array-ii.go
@@ -8,13 +8,41 @@ func search(nums []int, target int) bool {
 		return false
 	}
 
+
 }
 
 func findPivotIndex(nums []int) int {
 	left := 0
 	right := length(nums) - 1
 
+	for left < right {
+		mid := left + (right - left) / 2
+
+		// Check if mid is the pivot index
+		if mid > 0 && nums[mid] < nums[mid - 1]{
+			return mid
+		}
+
+		// Check if mid + 1 is the pivot index
+		if mid < length(nums) - 1 && nums[mid] > nums[mid + 1]{
+			return mid + 1
+		}
+
+		// If the pivot is in the right half, search in the right
+		if nums[mid] > nums[right]{
+			left = mid + 1
+		}else{
+			right = mid - 1
+		}
+	
+	return -1
+
 }
+
 func main() {
+	
+	array := []int{2,5,6,0,0,1,2}
+	findPivotIndex(array)
+
 
 }
