3f0649b43c4920dd72b098a9f95934b9db829131
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-17 17:57:36 +0530

    leetcode accepted

--- decode-ways.go
+++ decode-ways.go
@@ -2,7 +2,14 @@
 
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
+
+	fmt.Println(numDecodings("06"))
 
 }
 
@@ -10,33 +17,34 @@ func numDecodings(s string) int {
 
 	n := len(s)
 
-	if n < 1 || n > 100{
+	if n < 1 || n > 100 {
 		return 0
 	}
 
-	dp = make([]int, 0, n+1)
+	dp := make([]int, n+1)
 
 	dp[0] = 1
 
-	if s[0] != '0'{
-    	dp[1] = 1  
-	else {
+	if s[0] != '0' {
+		dp[1] = 1
+	} else {
 		dp[0] = 0
 	}
-	
-	for i:=2; i <= n;i++ {
-		oneDigit = strconv.parseInt(s[i - 1])
-		twoDigits = strconv.parseInt(s[i - 2 : i])
 
-		if oneDigit >= 1 and oneDigit <= 9{
-			dp[i] = dp[i] + dp[i - 1]
+	for i := 2; i < n+1; i++ {
+
+		oneDigit, _ := strconv.ParseInt(string(s[i-1]), 10, 32)
+		twoDigits, _ := strconv.ParseInt(string(s[i-2:i]), 10, 32)
+
+		if oneDigit >= 1 && oneDigit <= 9 {
+			dp[i] = dp[i] + dp[i-1]
 		}
 
-		if twoDigits >= 10 and twoDigits <= 26{
-			dp[i] = dp[i] + dp[i - 2]
+		if twoDigits >= 10 && twoDigits <= 26 {
+			dp[i] = dp[i] + dp[i-2]
 		}
 
 	}
-	
+
 	return dp[n]
 }
