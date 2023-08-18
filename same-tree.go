751b03c5a09add93ec02a3133e3c3e69ad7965ff
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-18 10:14:11 +0530

    same tree or not?

---
+++ same-tree.go
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/same-tree
+
+package main
+
+func main() {
+
+}
+
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+
+	if p == null && q == null {
+		return true
+	}
+
+	if p == null || q == null {
+		return false
+	}
+
+}
