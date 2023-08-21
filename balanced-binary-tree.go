11437e310dfb7217480433e96e51415354a5e7b0
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-21 09:53:22 +0530

    seems working

--- balanced-binary-tree.go
+++ balanced-binary-tree.go
@@ -15,50 +15,56 @@ type TreeNode struct {
 func main() {
 	root := &TreeNode{
 		Val: 1,
-		Left: &TreeNode{
+		left: &TreeNode{
 			Val: 2,
-			Left: &TreeNode{
+			left: &TreeNode{
 				Val:  3,
-				Left: &TreeNode{Val: 4},
+				left: &TreeNode{Val: 4},
 			},
 		},
-		Right: &TreeNode{Val: 5},
+		right: &TreeNode{Val: 5},
 	}
 	fmt.Println(isBalanced(root)) // Output: 2
 }
 
 func isBalanced(root *TreeNode) bool {
-	if root ==   nil{
-        return true
+	if root == nil {
+		return true
 	}
-    
-    leftHeight := getHeight(root.left)
-    rightHeight := getHeight(root.right)
-    
-    if abs(leftHeight - rightHeight) > 1{
-        return false
+
+	leftHeight := getHeight(root.left)
+	rightHeight := getHeight(root.right)
+
+	if abs(leftHeight-rightHeight) > 1 {
+		return false
 	}
-    
-    return isBalanced(root.left) AND isBalanced(root.right)
+
+	return isBalanced(root.left) && isBalanced(root.right)
 }
 
-
-
-function getHeight(node *TreeNode){
-    if node == nil {
-        return 0
+func getHeight(node *TreeNode) int {
+	if node == nil {
+		return 0
 	}
-    
-    leftHeight := getHeight(node.left)
-    rightHeight := getHeight(node.right)
-    
-    return 1 + max(leftHeight, rightHeight)
+
+	leftHeight := getHeight(node.left)
+	rightHeight := getHeight(node.right)
+
+	return 1 + max(leftHeight, rightHeight)
 
 }
 
-func abs( a int ) int {
-	if a < 0{
-		return -1*a
+func abs(a int) int {
+	if a < 0 {
+		return -1 * a
+	}
+
+	return a
+}
+
+func max(a, b int) int {
+	if a < b {
+		return b
 	}
 
 	return a
