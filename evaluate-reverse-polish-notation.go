a52db6f0acd9c3e639db36480b24c77953d38981
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-22 12:36:18 +0530

    add code

---
+++ evaluate-reverse-polish-notation.go
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/
+
+package main
+
+type Stack struct {
+	items []byte {}
+}
+
+func (s *Stack) Push(char byte) {
+	s.items = append(s.items, char)
+}
+
+func main() {
+
+}
