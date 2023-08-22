e991bb6114303fcb32b52f1258afdfa47c565e96
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-22 18:20:08 +0530

    used a map of strings

--- word-break.go
+++ word-break.go
@@ -4,12 +4,11 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	s := "leetycode"
-	wordDict := []string{"leet", "code"}
+	s := "catsandog"
+	wordDict := []string{"cats", "dog", "sand", "and", "cat"}
 	fmt.Println(wordBreak(s, wordDict))
 
 }
@@ -18,12 +17,18 @@ func wordBreak(s string, wordDict []string) bool {
 
 	n := len(s)
 	dp := make([]bool, n+1)
+	wordSet := make(map[string]bool)
+
+	for _, word := range wordDict {
+		wordSet[word] = true
+	}
+
 	dp[0] = true
 
 	for i := 0; i <= n; i++ {
 		for j := 0; j <= i; j++ {
 			for k := 0; k < len(wordDict); k++ {
-				if dp[j] == true && strings.Contains(wordDict[k], s[j:i]) {
+				if dp[j] == true && wordSet[s[j:i]] {
 
 					dp[i] = true
 					break
