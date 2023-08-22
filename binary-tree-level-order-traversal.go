403650bf21786e8762a86e6441556101ca6ac24a
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-22 08:50:24 +0530

    q definitions

---
+++ binary-tree-level-order-traversal.go
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/binary-tree-level-order-traversal/
+
+package main
+
+type Queue struct {
+	items []interface{}
+}
+
+func (q *Queue) Enqueue(item interface{}) {
+	q.items = append(q.items, item)
+}
+
+func (q *Queue) Dequeue() interface{} {
+	if len(q.items) == 0 {
+		return nil
+	}
+
+	item := q.items[0]
+	q.items = q.items[1:]
+
+	return item
+}
+
+func main() {
+
+}
