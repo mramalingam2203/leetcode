57646b078cf18165f419ea663dfd2ed8c8febc8b
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-22 10:28:51 +0530

    accepted by leetcode

--- binary-tree-zigzag-level-order-traversal.go
+++ binary-tree-zigzag-level-order-traversal.go
@@ -4,7 +4,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 )
 
 type TreeNode struct {
@@ -43,55 +42,33 @@ func levelOrder(root *TreeNode) [][]int {
 	}
 
 	queue := []*TreeNode{root}
-	var nTreeNodes int = 0
-	for len(queue) > 0 {
+	level := 0
 
+	for len(queue) > 0 {
 		levelSize := len(queue)
-		levelValues := make([]int, 0, levelSize)
+		levelValues := make([]int, levelSize)
 
 		for i := 0; i < levelSize; i++ {
 			current := queue[0]
 			queue = queue[1:]
 
-			if current.Val < -1000 || current.Val > 1000 {
-				fmt.Println("Invalid node value")
-				os.Exit(0)
+			if level%2 == 0 {
+				levelValues[i] = current.Val
+			} else {
+				levelValues[levelSize-1-i] = current.Val
 			}
 
-			levelValues = append(levelValues, current.Val)
-			
-			if i % 2 != 0
-				if current.Left != nil {
-					queue = append(queue, current.Left)
-					nTreeNodes++
-				}
-				if current.Right != nil {
-					queue = append(queue, current.Right)
-					nTreeNodes++
-				}
+			if current.Left != nil {
+				queue = append(queue, current.Left)
 			}
-
-
-			if i % 2 == 0
-				if current.Right != nil {
-					queue = append(queue, current.Right)
-					nTreeNodes++
-				}
-				if current.Left != nil {
-					queue = append(queue, current.Left)
-					nTreeNodes++
-				}
-			}
-
-			if nTreeNodes > 2000 {
-				fmt.Println("tree too long")
-				os.Exit(0)
+			if current.Right != nil {
+				queue = append(queue, current.Right)
 			}
 		}
 
 		result = append(result, levelValues)
+		level++
 	}
 
 	return result
-
 }
