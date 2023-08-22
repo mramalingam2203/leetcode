d1573f66afc1b78d3b19118d17c20c5ca81d3c45
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-22 18:13:21 +0530

    works locally

--- word-break.go
+++ word-break.go
@@ -2,7 +2,15 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 func main() {
+	s := "leetycode"
+	wordDict := []string{"leet", "code"}
+	fmt.Println(wordBreak(s, wordDict))
 
 }
 
@@ -13,13 +21,18 @@ func wordBreak(s string, wordDict []string) bool {
 	dp[0] = true
 
 	for i := 0; i <= n; i++ {
-		for j:=0; j <= i; j++{
-			if dp[j] == true && s[j:i] {
+		for j := 0; j <= i; j++ {
+			for k := 0; k < len(wordDict); k++ {
+				if dp[j] == true && strings.Contains(wordDict[k], s[j:i]) {
 
-				dp[i] = true
-				break
+					dp[i] = true
+					break
+				}
 			}
 
 		}
+	}
+
 	return dp[n]
+
 }
