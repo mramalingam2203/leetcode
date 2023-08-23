81b121ce82da251cd774276c274544a889c69efc
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-23 08:50:58 +0530

    works locally

--- triangle.go
+++ triangle.go
@@ -2,9 +2,14 @@
 
 package main
 
+import "fmt"
+
 func main() {
-	array := [][]int{{1}, {1, 2, 3}, {3, 4, 5, 6}}
-	minimumTotal(array)
+	array := [][]int{{2},
+		{3, 4},
+		{6, 5, 7},
+		{4, 1, 8, 3}}
+	fmt.Println(minimumTotal(array))
 }
 
 func minimumTotal(triangle [][]int) int {
@@ -17,11 +22,26 @@ func minimumTotal(triangle [][]int) int {
 		dp[i] = make([]int, n)
 	}
 
-	for i := 0; i <= n; i++ {
+	for i := 0; i < n; i++ {
 		dp[n-1][i] = triangle[n-1][i]
 	}
 
-	for i := n - 2; i > 0; i-- {
+	for i := n - 2; i >= 0; i-- {
+		for j := 0; j <= i; j++ {
 
+			dp[i][j] += triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
+
+		}
 	}
+
+	return dp[0][0]
+
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+
+	return b
 }
