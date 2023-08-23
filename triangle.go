8072e9f2625333994b98a4f80ca28d6a18670439
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-23 08:55:43 +0530

    leetcode accepted. constraints added

--- triangle.go
+++ triangle.go
@@ -16,6 +16,10 @@ func minimumTotal(triangle [][]int) int {
 
 	n := len(triangle) // number of rows
 
+	if n < 1 || n > 200 || len(triangle[0]) != 1 {
+		return 0
+	}
+
 	dp := make([][]int, n)
 
 	for i := 0; i < n; i++ {
@@ -23,12 +27,19 @@ func minimumTotal(triangle [][]int) int {
 	}
 
 	for i := 0; i < n; i++ {
+
+		if len(triangle[i]) != len(triangle[i-1])+1 {
+			return 0
+		}
+
 		dp[n-1][i] = triangle[n-1][i]
 	}
 
 	for i := n - 2; i >= 0; i-- {
 		for j := 0; j <= i; j++ {
-
+			if triangle[i][j] < -1e4 || triangle[i][j] > 1e4 {
+				return 0
+			}
 			dp[i][j] += triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1])
 
 		}
