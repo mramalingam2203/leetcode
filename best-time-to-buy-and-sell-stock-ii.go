d13a1ff21d7b79ada7188cd17d2ccbba195b4e2e
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-23 11:09:19 +0530

    works locally

--- best-time-to-buy-and-sell-stock-ii.go
+++ best-time-to-buy-and-sell-stock-ii.go
@@ -6,22 +6,23 @@ import "fmt"
 
 func main() {
 
-	array := []int{7, 6, 4, 3, 1}
+	array := []int{7, 1, 5, 3, 6, 4}
 	fmt.Println(maxProfit(array))
 
 }
 
-func maxProfit(prices){
-    if prices.length < 2:
-        return 0
-    
-    maxProfit := 0
-    
-    for i := 1; i< len(prices)- 1; i++ {
-        if prices[i] > prices[i - 1]{
-            maxProfit += prices[i] - prices[i - 1]
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+
+	maxProfit := 0
+
+	for i := 1; i < len(prices)-1; i++ {
+		if prices[i] > prices[i-1] {
+			maxProfit += prices[i] - prices[i-1]
 		}
 	}
 
-    return maxProfit
+	return maxProfit
 }
