572c57a8bf95dfc7808f6bcb786d217c8e5f8a20
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-23 09:37:48 +0530

    working locally

---
+++ pascals-triangle.go
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/pascals-triangle/
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+
+	fmt.Println(generate(5))
+
+}
+
+func generate(numRows int) [][]int {
+
+	if numRows == 0 {
+		fmt.Println("No rows")
+		os.Exit(0)
+	}
+
+	triangle := make([][]int, numRows)
+
+	for i := 0; i < numRows; i++ {
+		triangle[i] = make([]int, i+1)
+
+		triangle[i][0] = 1
+		triangle[i][i] = 1
+
+		for j := 1; j < i; j++ {
+			triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+		}
+	}
+
+	return triangle
+}
