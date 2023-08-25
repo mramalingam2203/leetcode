f896747cd3c0981fe8ce7c2fdc03c1f965502062
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-25 09:14:30 +0530

    code added

--- sum-root-to-leaf-numbers.go
+++ sum-root-to-leaf-numbers.go
@@ -16,6 +16,21 @@ func sumNumbers(root *TreeNode) int {
 
 func dfs(node *TreeNode, currentNum int) {
 
+	if node == nil {
+		return 0
+	}
+
+	currentSum := currentSum*10 + node.Val // Concatenate the current node value to the currentSum
+
+	if node.Left != nil && node.Right != nil {
+		return currentSum
+	}
+
+	leftSum := dfs(node.Left, currentSum)
+	rightSum := dfs(node.Right, currentSum)
+
+	return leftSum + rightSum
+
 }
 
 func main() {
