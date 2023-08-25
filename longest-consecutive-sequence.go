f19111a1446b91cb512cffaac8eabb472903c7c0
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-25 08:40:11 +0530

    code errors

--- longest-consecutive-sequence.go
+++ longest-consecutive-sequence.go
@@ -2,12 +2,19 @@
 
 package main
 
+import(
+	"fmt"
+	"github.com/deckarep/golang-set"
+)
+
 func main() {
 
 	array := []int{0, 3, 7, 2, 5, 8, 4, 6, 0, 1}
 	longestConsecutive(array)
 
 }
+
+longestStreak := 0
 
 func longestConsecutive(nums []int) int {
 	numSet := mapset.NewSet()
@@ -17,17 +24,24 @@ func longestConsecutive(nums []int) int {
 		numSet.Add(num)
 	}
 
-	for _, num := range numSet {
+	for _, num := range nums {
 		if !numSet.Contains(num - 1) {
-			currentNum = num
-			currentStreak = 1
+			currentNum := num
+			currentStreak := 1
 
 			for numSet.Contains(currentNum + 1) {
 
-				currentNum = currentNum + 1
-				currentStreak = currentStreak + 1
+				currentNum := currentNum + 1
+				currentStreak := currentStreak + 1
 
 			}
 		}
+		longestStreak = max(longestStreak, currentStreak)
 	}
 }
+
+func max (a, b int)int{
+		if a > b{
+				return a}
+				return b
+		}
