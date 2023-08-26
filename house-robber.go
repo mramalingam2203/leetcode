3e01e904a926495ce050ad35cea63ea8299fa0d5
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-26 08:41:27 +0530

    dp formulated

--- house-robber.go
+++ house-robber.go
@@ -8,4 +8,16 @@ func main() {
 
 func rob(nums []int) int {
 
+	n := len(nums)
+
+	dp := make([]int, n)
+
+	dp[0] = nums[0]
+	dp[1] = nums[1]
+
+	for i := 2; i < n; i++ {
+		dp[i] = max(dp[i-1], dp[i-2] + nums[i])
+
+	return dp[n-1]
+
 }
