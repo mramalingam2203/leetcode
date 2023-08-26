9ca58f2245cc350fc0243e5e703bfe469f049bb8
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-26 07:42:17 +0530

    wrong approach earlier

--- rotate-array.go
+++ rotate-array.go
@@ -7,12 +7,42 @@ import "fmt"
 func main() {
 
 	array := []int{1, 2, 3, 4, 5, 6, 7}
-	rotate(array, 3)
+
+	reverse(array, 0, len(array)-1)
+	//rotate(array, 1)
+	fmt.Println(array)
 
 }
 
+/*
+
 func rotate(nums []int, k int) {
 
-	fmt.Println(nums[0:k])
+	n := len(nums)
+
+	if n < 1 || n > 1e5 || k < 0 || k > 1e5 {
+		return
+	}
+
+	s1 := nums[n-k : n]
+	s2 := nums[:k+1]
+
+	for i := 0; i < len(s2); i++ {
+		s1 = append(s1, s2[i])
+	}
+
+	for i := 0; i < len(nums); i++ {
+		nums[i] = s1[i]
+	}
+}
+*/
+
+func reverse(nums []int, start, end int) {
+
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
 
 }
