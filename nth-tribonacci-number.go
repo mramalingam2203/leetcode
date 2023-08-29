60b43c95b0fd3e054ab3074f69f0d0e71ea5c580
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-29 10:23:23 +0530

    tribo solved

---
+++ nth-tribonacci-number.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/n-th-tribonacci-number
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(tribonacci(25))
+}
+
+func tribonacci(n int) int {
+
+	if n <= 0 || n > 37 {
+		return 0
+	}
+
+	trib_table := make([]int, n+1)
+	trib_table[0] = 0
+	trib_table[1] = 1
+	trib_table[2] = 1
+
+	for i := 3; i <= n; i++ {
+		trib_table[i] = trib_table[i-1] + trib_table[i-2] + trib_table[i-3]
+	}
+
+	return trib_table[n]
+}
