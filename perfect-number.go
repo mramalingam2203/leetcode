5ab7ff9db923b1eb0d809e0d88b1dc97ed6034be
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-29 18:14:04 +0530

    initi code

---
+++ perfect-number.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/perfect-number/
+
+func main() {
+
+}
+
+func checkPerfectNumber(num int) bool {
+
+}
+
+func findDivisorSum(num int) int {
+	divisors := []int{}
+
+	for i := 1; i <= int(math.Sqrt(float64(num)))+1; i++ {
+		if num % i == 0 {
+			divisors = append(divisors,i){
+				if i != num/i{
+					divisors.append(n/i)
+				}
+			}
+	}
+}
+
+fmt.Println(divisors)
+}
