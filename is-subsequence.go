f79d6cec546f326511cccc428787b680a40b6c53
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-29 12:07:06 +0530

    check substrings

---
+++ is-subsequence.go
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/is-subsequence/
+
+package main
+
+func main() {
+
+}
+
+func isSubsequence(s string, t string) bool {
+	i := 0
+	j := 0
+
+	for i < len(s) && j < len(t) {
+		if s[i] == t[j] {
+			i++
+		}
+		j++
+	}
+
+	return i == len(s)
+}
