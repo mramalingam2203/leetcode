20a47c6d4fbc4259fef4aafa6254b80c8b0378dd
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-29 17:59:09 +0530

    base 7 complete

--- base-7.go
+++ base-7.go
@@ -9,18 +9,46 @@ import (
 )
 
 func main() {
-	fmt.Println(convertToBase7(100))
+	convertToBase7(-7)
+}
+
+// function, which takes a string as
+// argument and return the reverse of string.
+func reverse(s string) string {
+	rns := []rune(s) // convert to rune
+	for i, j := 0, len(rns)-1; i < j; i, j = i+1, j-1 {
+
+		// swap the letters of the string,
+		// like first with last and so on.
+		rns[i], rns[j] = rns[j], rns[i]
+	}
+
+	// return the reversed string.
+	return string(rns)
 }
 
 func convertToBase7(num int) string {
-	if num < -1e7 || num > 1e7 {
+	if num < -1e7 || num > 1e7 || num == 0 {
 		return "0"
 	}
 	rems := []string{}
+	var sign bool
+
+	if num < 0 {
+		num *= -1
+		sign = true
+	}
 	for num != 0 {
 		rems = append(rems, strconv.Itoa(num%7))
 		num /= 7
 	}
 
-	return strings.Join(rems, "")
+	result := reverse(strings.Join(rems, ""))
+
+	if sign == true {
+		result = "-" + result
+	}
+
+	fmt.Println(result)
+	return result
 }
