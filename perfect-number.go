4732583175ced469662f0640168860938c6a0402
mramalingam2203 <dr.muthukumaran@live.com>
2023-08-29 18:21:52 +0530

    perfect number done

--- perfect-number.go
+++ perfect-number.go
@@ -1,25 +1,35 @@
 // https://leetcode.com/problems/perfect-number/
 
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
+
+	fmt.Println(checkPerfectNumber(28))
 
 }
 
 func checkPerfectNumber(num int) bool {
-
-}
-
-func findDivisorSum(num int) int {
 	divisors := []int{}
 
 	for i := 1; i <= int(math.Sqrt(float64(num)))+1; i++ {
-		if num % i == 0 {
-			divisors = append(divisors,i){
-				if i != num/i{
-					divisors.append(n/i)
-				}
+		if num%i == 0 {
+			divisors = append(divisors, i)
+			if i != num/i {
+				divisors = append(divisors, num/i)
 			}
+		}
 	}
-}
 
-fmt.Println(divisors)
+	sum := 0
+	for i := range divisors {
+		sum += divisors[i]
+	}
+
+	return sum-num == num
+
 }
