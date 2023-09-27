030095306df50e30750aa4904609f0a5c5fc1b84
mramalingam2203 <dr.muthukumaran@live.com>
2023-09-27 08:36:04 +0530

    works local

--- remove-invalid-parentheses.go
+++ remove-invalid-parentheses.go
@@ -9,6 +9,40 @@ func main() {
 
 func removeInvalidParentheses(s string) []string {
 
+	if s == "" {
+		return []string{""}
+	}
+
+	result := []string{}
+	visited := make(map[string]bool)
+	queue := []string{s}
+	found := false
+
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+
+		if isValid(current) {
+			result = append(result, current)
+			found = true
+		}
+
+		if found {
+			continue
+		}
+
+		for i, char := range current {
+			if char == '(' || char == ')' {
+				newStr := current[:i] + current[i+1:]
+				if _, exists := visited[newStr]; !exists {
+					visited[newStr] = true
+					queue = append(queue, newStr)
+				}
+			}
+		}
+
+	}
+	return result
 }
 
 func isValid(s string) bool {
