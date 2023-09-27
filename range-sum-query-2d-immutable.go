c0c19870efbe171c994894557686ec2e8db23d9a
mramalingam2203 <dr.muthukumaran@live.com>
2023-09-27 09:16:39 +0530

    guess will work on lc

--- range-sum-query-2d-immutable.go
+++ range-sum-query-2d-immutable.go
@@ -1,12 +1,14 @@
 // https://leetcode.com/problems/range-sum-query-2d-immutable/
 
+package main
+
 type NumMatrix struct {
 	elements [][]int
 }
 
 func Constructor(matrix [][]int) NumMatrix {
-	return NumArray{
-		elements: nums,
+	return NumMatrix{
+		elements: matrix,
 	}
 
 }
@@ -15,7 +17,7 @@ func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
 	sum := 0
 	for row := row1; row <= row2; row++ {
 		for col := col1; col <= col2; col++ {
-			sum += matrix[row][col]
+			sum += this.elements[row][col]
 		}
 	}
 	return sum
