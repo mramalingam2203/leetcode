c8417b60cd83bdca005bb170d737c09c0b8c7662
mramalingam2203 <dr.muthukumaran@live.com>
2023-09-27 08:20:39 +0530

    valid string code

--- remove-invalid-parentheses.go
+++ remove-invalid-parentheses.go
@@ -13,4 +13,19 @@ func removeInvalidParentheses(s string) []string {
 
 func isValid(s string) bool {
 
+	count := 0
+
+	for char := range s {
+
+		if char == '(' {
+			count++
+		} else if char == ')' {
+			count--
+			if count < 0 {
+				return false
+			}
+
+		}
+	}
+	return count == 0
 }
