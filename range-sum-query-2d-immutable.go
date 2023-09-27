2d14b14b7d7cb7a3de4f1ebceb911b98dbfaab14
mramalingam2203 <dr.muthukumaran@live.com>
2023-09-27 09:13:19 +0530

    defined function to add elements

---
+++ range-sum-query-2d-immutable.go
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/range-sum-query-2d-immutable/
+
+type NumMatrix struct {
+}
+
+func Constructor(matrix [][]int) NumMatrix {
+
+}
+
+func (this *NumMatrix) SumRegion(row1 int, col1 int, row2 int, col2 int) int {
+	sum := 0
+	for row := row1; row <= row2; row++ {
+		for col := col1; col <= col2; col++ {
+			sum += matrix[row][col]
+		}
+	}
+	return sum
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * obj := Constructor(matrix);
+ * param_1 := obj.SumRegion(row1,col1,row2,col2);
+ */
