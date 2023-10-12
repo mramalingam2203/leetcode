de4255e088f081c10cb553b1e445466f69df3387
mramalingam2203 <dr.muthukumaran@live.com>
2023-10-12 09:07:31 +0530

    lc run cases accepted

--- contiguous-array.go
+++ contiguous-array.go
@@ -5,8 +5,23 @@ package main
 import "fmt"
 
 func main() {
-	array := []int{0, 1, 0, 1, 1, 0, 1, 1}
-	fmt.Println(generateSubarrays(array))
+	array := []int{0, 1, 0}
+	fmt.Println(findMaxLength(array))
+}
+
+func findMaxLength(nums []int) int {
+	subarrays := generateSubarrays(nums)
+	result := 0
+	var k int
+	for subarray := range subarrays {
+		k = isContiguous((subarrays[subarray]))
+		if k > result {
+			result = k
+		}
+	}
+
+	return result
+
 }
 
 func generateSubarrays(arr []int) [][]int {
@@ -21,4 +36,23 @@ func generateSubarrays(arr []int) [][]int {
 	}
 
 	return subarrays
+}
+
+func isContiguous(arr []int) int {
+
+	count_0 := 0
+	count_1 := 0
+
+	for digit := range arr {
+		if digit == 0 {
+			count_0++
+		} else {
+			count_1++
+		}
+	}
+	if count_0 == count_1 {
+		return len(arr)
+	}
+
+	return -1
 }
